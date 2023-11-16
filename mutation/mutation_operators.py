d26ac0e5f94675eac8caa326eb9bb32c62dda4a6
Deep <patelbhai2981@gmail.com>
2023-11-15 21:15:40 -0500

    updates

--- mutation/mutation_operators.py
+++ mutation/mutation_operators.py
@@ -2,7 +2,6 @@ from copy import deepcopy
 import random
 
 class MutationOperators:
-    @staticmethod
     def mutate_coefficient(coefficient, index=0):
         mutation_type = random.choice(["replace", "scale", "add"])
         
@@ -17,23 +16,19 @@ class MutationOperators:
 
         return mutated_coefficient
 
-    @staticmethod
     def mutate_arithmetic_operation(poly, operation):
         mutated_poly = deepcopy(poly)
         mutated_poly.coefficients = [operation(coef) for coef in mutated_poly.coefficients]
         return mutated_poly
 
-    @staticmethod
     def mutate_to_zero_coefficient(poly, index):
         mutated_poly = deepcopy(poly)
         mutated_poly.coefficients[index] = 0
         return mutated_poly
 
-    @staticmethod
     def introduce_redundant_code(coefficients):
         return [coef * 1 for coef in coefficients]
 
-    @staticmethod
     def mutate_degree(poly):
         mutated_poly = deepcopy(poly)
         mutated_poly.coefficients.insert(1, 1)
